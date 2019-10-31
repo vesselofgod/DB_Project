e58db5103f314d8d6c425b104e4d987f5ec165ff
Lee Jisoo <54274442+tojslee@users.noreply.github.com>
2019-10-31 13:27:04 +0900

    Update problem2.sql

--- Project1/problem2.sql
+++ Project1/problem2.sql
@@ -1,15 +1,31 @@
-select distinct c1."COURSE_NAME"
+select "COURSE_NAME"
+from(
+select c."COURSE_NAME",
+	CASE
+	WHEN sum is null
+	THEN 0
+	ELSE sum
+	END AS total
 from
-(select A."COURSE_ID_PREFIX", A."COURSE_ID_NO", all_students - one_students as again
+(select  distinct "COURSE_ID_PREFIX", "COURSE_ID_NO", "COURSE_NAME"
+from "COURSE"
+) as c
+left outer join 
+(select "COURSE_ID_PREFIX", "COURSE_ID_NO", sum(reg)
+from(
+select "COURSE_ID_PREFIX", "COURSE_ID_NO", "STUDENT_ID", count(*) as reg
+from(
+select  c."COURSE_ID", "STUDENT_ID", count(*)
 from
-(select "COURSE_ID_PREFIX", "COURSE_ID_NO", count(*) as all_students
+(select "COURSE_ID_PREFIX", "COURSE_ID_NO", count(*)
 from "COURSE" as c, "COURSE_REGISTRATION" as cr
 where c."COURSE_ID" = cr."COURSE_ID"
-GROUP BY "COURSE_ID_PREFIX", "COURSE_ID_NO") as A,
-(select "COURSE_ID_PREFIX", "COURSE_ID_NO", count(distinct "STUDENT_ID") as one_students
-from "COURSE" as c, "COURSE_REGISTRATION" as cr
-where c."COURSE_ID" = cr."COURSE_ID"
-GROUP BY "COURSE_ID_PREFIX", "COURSE_ID_NO") as B
-where A."COURSE_ID_PREFIX" = B."COURSE_ID_PREFIX" and A."COURSE_ID_NO" = B."COURSE_ID_NO"
-order by again desc limit 3) as final, "COURSE" as c1
-where final."COURSE_ID_PREFIX" = c1."COURSE_ID_PREFIX" and final."COURSE_ID_NO" = c1."COURSE_ID_NO"
+group by "COURSE_ID_PREFIX", "COURSE_ID_NO") as first, "COURSE" as c, "COURSE_REGISTRATION" as cr
+where first."COURSE_ID_PREFIX" = c."COURSE_ID_PREFIX" and first."COURSE_ID_NO" = c."COURSE_ID_NO" and c."COURSE_ID" = cr."COURSE_ID"
+group by c."COURSE_ID", "STUDENT_ID") as second, "COURSE" as c
+where second."COURSE_ID" = c."COURSE_ID"
+group by "COURSE_ID_PREFIX", "COURSE_ID_NO", "STUDENT_ID"
+order by "COURSE_ID_PREFIX", "COURSE_ID_NO") as third
+where reg > 1
+group by "COURSE_ID_PREFIX", "COURSE_ID_NO") as fourth on fourth."COURSE_ID_PREFIX" = c."COURSE_ID_PREFIX" and fourth."COURSE_ID_NO" = c."COURSE_ID_NO"
+order by total desc limit 3) as fifth
