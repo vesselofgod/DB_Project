361b0e632096968c4f27dbd41dfec4a0c7f0d50a
Lee Jisoo <54274442+tojslee@users.noreply.github.com>
2019-11-22 16:27:18 +0900

    Update Project2.java

--- Project2/src/Project2.java
+++ Project2/src/Project2.java
@@ -1,3 +1,4 @@
+  
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -5,102 +6,36 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
+import java.util.Scanner;
 
 public class Project2 {
-  private static final String DB_DRIVER = "org.postgresql.Driver";
-  private static final String DB_CONNECTION_URL = "jdbc:postgresql://127.0.0.1/postgres";
-  private static final String DB_USER = "postgres";
-  private static final String DB_PASSWORD = "yp2207";
-
   public static void main(String args[]) throws ClassNotFoundException, SQLException {
-    Class.forName(DB_DRIVER);
-    Properties props = new Properties();
-
-    /* Setting Connection Info */
-    props.setProperty("user", DB_USER);
-    props.setProperty("password", DB_PASSWORD);
-    props.setProperty("characterEncoding", "UTF-8");
-    props.setProperty("serverTimezone", "Asia/Seoul");
-
-    /* Connect! */
-    Connection conn = DriverManager.getConnection(DB_CONNECTION_URL, props);
-
-    /* Create SQL statement object */
-    Statement st = conn.createStatement();
-
-    /* Create Table SQL */
-    String createTableSQL = "CREATE TABLE student_table " +
-                            "(ID int, " +
-                            "name varchar(20) not null, " +
-                            "address varchar(50) not null," +
-                            "department_ID int," +
-                            "primary key (ID))";
-    st.executeUpdate(createTableSQL);
-
-    /* Insert Row directly using raw statement */
-    String insertSQLDirectly = "INSERT INTO student_table values (1, 'Brandt', 'addr1', 1)";
-    st.executeUpdate(insertSQLDirectly);
-
-    /* Insert Row using PreparedStatement */
-    String insertSQLPrepared = "INSERT INTO student_table (ID, name, address, department_ID) values (?, ?, ?, ?)";
-
-    PreparedStatement preparedStmt = conn.prepareStatement(insertSQLPrepared);
-    preparedStmt.setInt(1, 2);
-    preparedStmt.setString(2, "Chavez");
-    preparedStmt.setString(3, "addr2");
-    preparedStmt.setInt(4, 2);
-    preparedStmt.execute();
-
-    /* Show current rows in student_table */
-    ResultSet rs = st.executeQuery("SELECT ID, name, address, department_ID FROM student_table");
-    System.out.println("============ RESULT ============");
-    while (rs.next()) {
-        System.out.print("ID : " + rs.getString(1) + ", ");
-        System.out.print("Name : " + rs.getString(2) + ", ");
-        System.out.print("Address : " + rs.getString(3) + ", ");
-        System.out.print("Department_ID : " + rs.getString(4));
-        System.out.println();
-    }
-
-    /* Update Row */
-    String updateSQL = "UPDATE student_table SET address = ? where ID = ?";
-    preparedStmt = conn.prepareStatement(updateSQL);
-    preparedStmt.setString(1, "addr3");
-    preparedStmt.setInt(2, 2);
-    preparedStmt.executeUpdate();
-
-    /* Show current rows in student_table */
-    rs = st.executeQuery("SELECT ID, name, address, department_ID FROM student_table");
-    System.out.println("============ RESULT ============");
-    while (rs.next()) {
-        System.out.print("ID : " + rs.getString(1) + ", ");
-        System.out.print("Name : " + rs.getString(2) + ", ");
-        System.out.print("Address : " + rs.getString(3) + ", ");
-        System.out.print("Department_ID : " + rs.getString(4));
-        System.out.println();
-    }
-
-    /* Delete Row */
-    String deleteSQL = "DELETE FROM student_table where ID = 2";
-    st.executeUpdate(deleteSQL);
-
-    /* Show current rows in student_table */
-    rs = st.executeQuery("SELECT ID, name, address, department_ID FROM student_table");
-    System.out.println("============ RESULT ============");
-    while (rs.next()) {
-        System.out.print("ID : " + rs.getString(1) + ", ");
-        System.out.print("Name : " + rs.getString(2) + ", ");
-        System.out.print("Address : " + rs.getString(3) + ", ");
-        System.out.print("Department_ID : " + rs.getString(4));
-        System.out.println();
-    }
-
-    /* Drop table */
-    String dropTableSQL = "DROP TABLE student_table";
-    st.executeUpdate(dropTableSQL);
-
-    preparedStmt.close();
-    st.close();
-    rs.close();
+	int instruction;
+	while(true) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Please input the instruction number (1: Import from CSV, 2: Export to CSV, 3: Manipulate Data, 4: Exit) :");
+		instruction = sc.nextInt();
+		switch(instruction) {
+		case 1: // Import from CSV
+			String file_name;
+			String CSV_file;
+			System.out.println("[Import from CSV]");
+			System.out.print("Please specify the filename for table description) : ");
+			file_name = sc.nextLine(); // 
+			System.out.println("Table is newly created as describled in the file");
+			System.out.print("Please specify the CSV filename : ");
+			CSV_file = sc.nextLine();
+			break;
+		case 2: // Export to CSV
+			System.out.println(2);
+			break;
+		case 3: // Manipulate Data
+			System.out.println(3);
+			break;
+		case 4: // Exit
+			System.out.println(4);
+			return;
+		}
+	}
   }
 }
