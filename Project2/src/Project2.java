24d43d18c1af796ec47cbbeaaa7b9d7ae72caaa5
Lee Jisoo <54274442+tojslee@users.noreply.github.com>
2019-11-22 16:46:00 +0900

    Update Project2.java

--- Project2/src/Project2.java
+++ Project2/src/Project2.java
@@ -8,26 +8,35 @@ import java.sql.Statement;
 import java.util.Properties;
 import java.util.Scanner;
 
-public class Project2 {
+public class project2_main {
   public static void main(String args[]) throws ClassNotFoundException, SQLException {
 	int instruction;
 	while(true) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Please input the instruction number (1: Import from CSV, 2: Export to CSV, 3: Manipulate Data, 4: Exit) :");
 		instruction = sc.nextInt();
+		String table_name; // 수정하거나 참조하는 table name
+		String file_name; // 참조하는 file name
+		String CSV_file; // 쓰거나 참조하는 CSV file namee
 		switch(instruction) {
 		case 1: // Import from CSV
-			String file_name;
-			String CSV_file;
 			System.out.println("[Import from CSV]");
 			System.out.print("Please specify the filename for table description) : ");
-			file_name = sc.nextLine(); // 
+			file_name = sc.nextLine();
 			System.out.println("Table is newly created as describled in the file");
 			System.out.print("Please specify the CSV filename : ");
 			CSV_file = sc.nextLine();
+			// 오류없이 돌아간 경우
+			System.out.println("Data import completed. (Insertion Success : ");
+			// 오류가 난 경우
+			// failure 개수만큼 loop돌려서 출력해주기
 			break;
 		case 2: // Export to CSV
-			System.out.println(2);
+			System.out.println("[Export to CSV]");
+			System.out.print("Please specify the table name : ");
+			table_name = sc.nextLine();
+			System.out.print("Please specify the CSV filename : ");
+			CSV_file = sc.nextLine();
 			break;
 		case 3: // Manipulate Data
 			System.out.println(3);
